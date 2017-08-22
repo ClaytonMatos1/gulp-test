3126474480e3fed802580508c059294153cc7a09
Clayton Matos <Clayton Matos>
2017-08-22 00:21:02 -0300

    [master] commit inicial

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    htmlReplace = require('gulp-html-replace'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin')
+    cssmin = require('gulp-cssmin'),
+    browserSync = require('browser-sync'),
+    jshint = require('gulp-jshint'),
+    jshintStylish = require('jshint-stylish'),
+    cssLint = require('gulp-csslint'),
+    autoprefixer = require('gulp-autoprefixer'),
+    less = require('gulp-less');
+
+gulp.task('default', ['copy'], function () {
+    gulp.start('build-img', 'usemin');
+});
+
+gulp.task('copy', ['clean'], function () {
+    return gulp.src('src/**/*')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function () {
+    return gulp.src('dist')
+        .pipe(clean());
+});
+
+gulp.task('build-img', function () {
+    gulp.src('dist/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function () {
+    gulp.src('dist/**/*.html')
+        .pipe(usemin({
+            'js' : [uglify],
+            'css' : [autoprefixer, cssmin]
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('server', function () {
+    browserSync.init({
+        server : {
+            baseDir : 'src'
+        }
+    });
+
+    gulp.watch('src/js/*.js').on('change', function (event) {
+        gulp.src(event.path)
+            .pipe(jshint())
+            .pipe(jshint.reporter(jshintStylish));
+    });
+
+    // gulp.watch('src/css/*.css').on('change', function (event) {
+    //     gulp.src(event.path)
+    //         .pipe(cssLint())
+    //         .pipe(cssLint.reporter());
+    // });
+
+    gulp.watch('src/less/*.less').on('change', function (event) {
+        gulp.src(event.path)
+            .pipe(less().on('error', function (err) {
+                console.log('problema no arquivo less', err);
+            }))
+            .pipe(gulp.dest('src/css'));
+    });
+
+    gulp.watch('src/**/*').on('change', browserSync.reload);
+});
+
+// gulp.task('build-js', function () {
+//     gulp.src(['dist/js/jquery.js', 'dist/js/home.js', 'dist/js/produto.js'])
+//         .pipe(concat('all.js'))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('dist/js'));
+// });
+
+// gulp.task('build-css', function () {
+//     gulp.src(['dist/css/reset.css', 'dist/css/estilos.css', 'dist/css/mobile.css'])
+//         .pipe(concat('all.css'))
+//         .pipe(gulp.dest('dist/css'));
+// });
+
+// gulp.task('replace-html', function () {
+//     gulp.src('dist/**/*.html')
+//         .pipe(htmlReplace({
+//             js : 'js/all.js',
+//             css : 'css/all.css'
+//         }))
+//         .pipe(gulp.dest('dist'));
+// });
